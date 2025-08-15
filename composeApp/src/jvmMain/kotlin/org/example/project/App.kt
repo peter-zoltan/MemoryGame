2c2f02c76bf99024c65ceb04e02f83e8d832e59e
peter-zoltan <peter.zoltan2004@gmail.com>
2025-08-15 12:54:11 +0200

    Moved most things to Card.kt Now generates buttons algorithmycally Separated the setuo of icons, column, row and button into their own functions

--- composeApp/src/jvmMain/kotlin/org/example/project/App.kt
+++ composeApp/src/jvmMain/kotlin/org/example/project/App.kt
@@ -1,101 +1,15 @@
 package org.example.project
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import androidx.compose.animation.*
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberOverscrollEffect
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
 
-
-import memorygame.composeapp.generated.resources.Res
-import memorygame.composeapp.generated.resources.*
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        val showContent = remember { List(4) { mutableStateOf(false) } }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize()
-                .verticalScroll(
-                    state = rememberScrollState(),
-                    overscrollEffect = rememberOverscrollEffect(),
-                ),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Row(
-                modifier = Modifier
-                    .background(MaterialTheme.colorScheme.primaryContainer)
-                    .safeContentPadding()
-                    .height(500.dp),
-                verticalAlignment = Alignment.Bottom,
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Button(
-                    onClick = { showContent[0].value = !showContent[0].value },
-                    shape = RectangleShape,
-                    colors = ButtonDefaults.buttonColors(Color.Black)
-                ) {
-                    if (showContent[0].value) {
-                        Image(painterResource(Res.drawable.fool), null)
-                    } else {
-                        Image(painterResource(Res.drawable.back), null)
-                    }
-                }
-
-                //Spacer(Modifier.width(16.dp))
-
-            }
-
-            Row(
-                modifier = Modifier
-                    .background(MaterialTheme.colorScheme.primaryContainer)
-                    .safeContentPadding()
-                    .height(500.dp),
-                verticalAlignment = Alignment.Bottom,
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Button(
-                    onClick = { showContent[1].value = !showContent[1].value },
-                    shape = RectangleShape,
-                    colors = ButtonDefaults.buttonColors(Color.Black)
-                ) {
-                    if (showContent[1].value) {
-                        Image(painterResource(Res.drawable.hermit), null)
-                    } else {
-                        Image(painterResource(Res.drawable.back), null)
-                    }
-                }
-            }
-        }
+        createLayout()
     }
 }
---
+++ composeApp/src/jvmMain/kotlin/org/example/project/Card.kt
@@ -0,0 +1,115 @@
+package org.example.project
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.rememberOverscrollEffect
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import memorygame.composeapp.generated.resources.*
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.painterResource
+
+fun setIcons() : List<DrawableResource> {
+    val icons = listOf(
+        Res.drawable.fool,
+        Res.drawable.hermit,
+        Res.drawable.hanged_man,
+        Res.drawable.justice,
+        Res.drawable.magician,
+        Res.drawable.moon,
+        Res.drawable.star,
+        Res.drawable.sun,
+        Res.drawable.world,
+    )
+    return icons
+}
+
+@Composable
+fun createLayout() {
+    val showContent = remember { List(9) { mutableStateOf(false) } }
+    val icons = setIcons()
+    setColumn {
+        setRow {
+            setButtons(5, 0, showContent, icons)
+        }
+        setRow {
+            setButtons(4, 5, showContent, icons)
+        }
+    }
+}
+
+@Composable
+fun setColumn (
+    columnContent : @Composable (ColumnScope.() -> Unit)
+) {
+    Column(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primaryContainer)
+            .safeContentPadding()
+            .fillMaxSize()
+            .verticalScroll(
+                state = rememberScrollState(),
+                overscrollEffect = rememberOverscrollEffect(),
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        content = columnContent,
+    )
+}
+
+@Composable
+fun setRow(
+    rowContent : @Composable (RowScope.() -> Unit)
+) {
+    Row(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primaryContainer)
+            .safeContentPadding()
+            .height(300.dp),
+        verticalAlignment = Alignment.Bottom,
+        horizontalArrangement = Arrangement.Center,
+        content = rowContent,
+    )
+}
+
+@Composable
+fun setButtons(
+    number : Int,
+    startIndex : Int,
+    showContent : List<MutableState<Boolean>>,
+    icons : List<DrawableResource>,
+) {
+    for (i in startIndex..<startIndex + number) {
+        Button(
+            onClick = { showContent[i].value = !showContent[i].value },
+            shape = RectangleShape,
+            colors = ButtonDefaults.buttonColors(Color.Black),
+        ) {
+            if (showContent[i].value) {
+                Image(painterResource(icons[i]), null)
+            } else {
+                Image(painterResource(Res.drawable.back), null)
+            }
+        }
+    }
+}
