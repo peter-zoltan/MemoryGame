1286703443f1aad13d65247ebef9f0ac19af6a1f
peter-zoltan <peter.zoltan2004@gmail.com>
2025-08-30 17:43:57 +0200

    separated ui elements to the file Layout.kt

--- composeApp/src/jvmMain/kotlin/org/example/project/Card.kt
+++ composeApp/src/jvmMain/kotlin/org/example/project/Card.kt
@@ -1,29 +1,6 @@
 package org.example.project
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.rememberOverscrollEffect
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -31,7 +8,6 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import memorygame.composeapp.generated.resources.*
 import org.jetbrains.compose.resources.DrawableResource
-import org.jetbrains.compose.resources.painterResource
 
 
 class Card(
@@ -133,73 +109,4 @@ fun setCards() : List<Card> {
         Card(Res.drawable.world),*/
     )
     return cards
-}
-
-
-@Composable
-fun createLayout() {
-    val cards = setCards()
-    val deck = remember { Deck(cards) }
-    setColumn {
-        setRow {
-            setButtons(4, 0, deck)
-        }
-        setRow {
-            setButtons(4, 4, deck)
-        }
-    }
-}
-
-
-@Composable
-fun setColumn (
-    columnContent : @Composable (ColumnScope.() -> Unit)
-) {
-    Column(
-        modifier = Modifier
-            .background(MaterialTheme.colorScheme.primaryContainer)
-            .safeContentPadding()
-            .fillMaxSize()
-            .verticalScroll(
-                state = rememberScrollState(),
-                overscrollEffect = rememberOverscrollEffect(),
-            ),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        content = columnContent,
-    )
-}
-
-
-@Composable
-fun setRow(
-    rowContent : @Composable (RowScope.() -> Unit)
-) {
-    Row(
-        modifier = Modifier
-            .background(MaterialTheme.colorScheme.primaryContainer)
-            .safeContentPadding()
-            .height(300.dp),
-        verticalAlignment = Alignment.Bottom,
-        horizontalArrangement = Arrangement.Center,
-        content = rowContent,
-    )
-}
-
-
-@Composable
-fun setButtons(
-    number : Int,
-    startIndex : Int,
-    deck : Deck,
-) {
-    for (i in startIndex..<startIndex + number) {
-        Button(
-            onClick = { deck.onClick(i) },
-            shape = RectangleShape,
-            colors = ButtonDefaults.buttonColors(Color.Black),
-        ) {
-            Image(painterResource(deck.cards[i].face.value), null)
-        }
-    }
 }
